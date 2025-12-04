b6ef367b40774fe9e95a6420db258f8482191b35
Fabrizio Parodi <fabrizio.parodi@ge.infn.it>
2025-12-04 13:38:20 +0100

    update

--- fitGausExp.C
+++ fitGausExp.C
@@ -1,14 +1,22 @@
 {
 
   ifstream file("gausexp.dat");
-  double min=100;
-  double max=160;
-  TH1D h("h","",30,min,max);
-  double val1,val2;
-  int i=1;
-  while (file >> val1 >> val2){
-    h.SetBinContent(i++,val2);
+  std::vector<double> x,n;
+  double x_tmp,n_tmp;
+  while (file >> x_tmp >> n_tmp){
+    x.push_back(x_tmp);
+    n.push_back(n_tmp);
   }
+  double dx   = x[1]-x[0];
+  int nbin    = x.size();
+  double xmin = x[0]-dx/2;
+  double xmax = x[x.size()-1]+dx/2;
+
+  TH1D h("h","",nbin,xmin,xmax);
+
+  int i=1;
+  for (auto xn: n)
+    h.SetBinContent(i++,xn);
 
   h.SetMarkerStyle(20);
   h.Draw("PE");
